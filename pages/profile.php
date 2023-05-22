e1bcc25527c22684039050a07ac7cd961e97ba5b
Artur Safin <rusgamehack@gmail.com>
2023-05-22 11:03:12 +0300

    fix-adaptive

--- pages/profile.php
+++ pages/profile.php
@@ -9,6 +9,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Личный кабинет</title>
     <link rel="stylesheet" href="profile.css">
     <link rel="stylesheet" href="../fonts/fonts.css">
